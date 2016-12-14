ede5a423d2ce696148ff7cfecb0c38a9dcaa24c2
Kenneth Black and Marty <kenneth+marty@>
2016-12-14 10:29:52 -0800

    add new recipe creation, tested

--- app.rb
+++ app.rb
@@ -3,11 +3,19 @@ require("sinatra/reloader")
 require('sinatra/activerecord')
 also_reload("lib/**/*.rb")
 require("pg")
-require('ingredient')
-require('recipe')
-require('tag')
+require('./lib/ingredient')
+require('./lib/recipe')
+require('./lib/tag')
 
 get('/') do
   @recipes = Recipe.all
   erb(:index)
+end
+
+post('/recipes') do
+  Recipe.create({
+    :name => params.fetch('new-recipe-name'),
+    :instruction => params.fetch('new-recipe-instruction')
+  })
+  redirect '/'
 end
--- spec/recpie_box_integration_spec.rb
+++ spec/recpie_box_integration_spec.rb
@@ -4,15 +4,22 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe("recipe box app", {:type => :feature}) do
-
-  describe("the view recipes path") do
-      it 'will let the user see a list of recipes' do
-        Recipe.create({:name => "Beef Wellington", :instruction => "has many"})
-        visit '/'
-        expect(page).to have_content "Beef Wellington"
-      end
+describe "recipe box app", {:type => :feature} do
+  describe "the view recipes path" do
+    it 'will let the user see a list of recipes' do
+      Recipe.create({:name => "Beef Wellington", :instruction => "has many"})
+      visit '/'
+      expect(page).to have_content "Beef Wellington"
+    end
   end
-
+  describe "the add recipes path" do
+    it "will let the user add new recipes" do
+      visit '/'
+      fill_in('new-recipe-name', :with => 'Scrambled Eggs')
+      fill_in('new-recipe-instruction', :with => 'few')
+      click_button('Create Recipe')
+      expect(page).to have_content "Scrambled Eggs"
+    end
+  end
 
 end
