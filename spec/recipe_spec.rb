c31bb42e943f96edb986b886c2f5cb6fe79a2963
Kenneth Black and Marty <marty@>
2016-12-14 09:45:27 -0800

    passing test for #recipes for Tag object, verify proper many to many db connection.

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -1,5 +1,11 @@
 require('spec_helper')
 
 describe Recipe do
-
+  describe '#ingredients' do
+    it "creates a many to one relationship properly" do
+      steak_pie = Recipe.create({:name => "Steak Pie", :instruction => "Many"})
+      beef = Ingredient.create({:name => "Beef", :amount => "2lb", :recipe_id => steak_pie.id})
+      expect(steak_pie.ingredients).to eq [beef]
+    end
+  end
 end
--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -1,5 +1,12 @@
 require('spec_helper')
 
 describe Tag do
-
+  describe '#recipes' do
+    it 'will establish the many to many relationship' do
+      steak_pie = Recipe.create({:name => "Steak Pie", :instruction => "Many"})
+      test_tag = Tag.create({:name => "meat"})
+      steak_pie.tags.push(test_tag)
+      expect(test_tag.recipes).to eq [steak_pie]
+    end
+  end
 end
